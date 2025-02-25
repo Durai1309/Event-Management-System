571d07c155beb3e0f3cf0ab4f7869e73139df996
Durai1309 <durairaj14092000@gmail.com>
2025-02-25 23:47:35 +0530

    Created the Domain,Infrastructure and Application in the project

---
+++ EventManagementSystem.Domain/Entities/Event.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventManagementSystem.Domain.Entities
+{
+    public class Event
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public double Price { get; set; }
+        public int Sqft { get; set; }
+        public int Occupancy { get; set; }
+        [NotMapped]
+        public string? ImageUrl { get; set; }
+        public DateTime? Created_Date { get; set; }
+        public DateTime? Updated_Date { get; set; }
+
+    }
+}
