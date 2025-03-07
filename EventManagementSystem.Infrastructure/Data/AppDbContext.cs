db6fb5a50cea2f937b38c7dbca57e4a066aec358
Durai1309 <durairaj14092000@gmail.com>
2025-03-07 23:36:41 +0530

    Created the IEevntRepository

--- EventManagementSystem.Infrastructure/Data/AppDbContext.cs
+++ EventManagementSystem.Infrastructure/Data/AppDbContext.cs
@@ -7,7 +7,7 @@ namespace EventManagementSystem.Infrastructure.Data
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
-            //4.28
+            //4.35
         }
         public DbSet<Event> Event { get; set; }
         public DbSet<EventNumber> EventNumbers { get; set; }
