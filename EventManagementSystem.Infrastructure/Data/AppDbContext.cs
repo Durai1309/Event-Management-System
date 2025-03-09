5505aa0654783a293b7e0a16cefced9430334da9
Durai1309 <durairaj14092000@gmail.com>
2025-03-09 14:15:14 +0530

    Created the Repository

--- EventManagementSystem.Infrastructure/Data/AppDbContext.cs
+++ EventManagementSystem.Infrastructure/Data/AppDbContext.cs
@@ -7,7 +7,7 @@ namespace EventManagementSystem.Infrastructure.Data
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
-            //4.35
+            //5.2
         }
         public DbSet<Event> Event { get; set; }
         public DbSet<EventNumber> EventNumbers { get; set; }
