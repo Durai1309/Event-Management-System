5923730e65e775a5553a1354838927c41e88e2f8
Durai1309 <durairaj14092000@gmail.com>
2025-03-15 16:44:40 +0530

    Worked on AppDbContext

--- EventManagementSystem.Domain/Entities/Booking.cs
+++ EventManagementSystem.Domain/Entities/Booking.cs
@@ -1,12 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 
 namespace EventManagementSystem.Domain.Entities
 {
-    internal class Booking
+    public class Booking
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string UserId { get; set; }
+        [ForeignKey("UserId")]
+        public ApplicationUser? User { get; set; }
+
+        [Required]
+        public int EventId { get; set; }
+        [ForeignKey("EventId")]
+        public Event Event { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Email { get; set; }
+        public string? Phone { get; set; }
+
+        [Required]
+        public double TotalCost { get; set; }
+        public int Nights { get; set; }
+        public string? Status { get; set; }
+
+        [Required]
+        public DateTime BookingDate { get; set; }
+        [Required]
+        public DateOnly CheckInDate { get; set; }
+        [Required]
+        public DateOnly CheckOutDate { get; set; }
+
+        public DateTime ActualCheckInDate { get; set; }
+        public DateTime ActualCheckOutDate { get; set; }
+
+        public int EventNumber { get; set; }
+
+        [NotMapped]
+        public List<EventNumber> EventNumbers { get; set; }
     }
 }
